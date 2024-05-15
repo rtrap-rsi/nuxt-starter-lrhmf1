565a74aba3966d24e1930e3f964ad14e11299477
rtrap-rsi <roberto.trapletti@rsi.ch>
2024-05-15 13:39:25 +0200

    feat: added index and about page

---
+++ app/layouts/default.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <div>
+      <NuxtLink to="/">Home</NuxtLink>
+      <NuxtLink to="/about">About</NuxtLink>
+    </div>
+    <slot />
+  </div>
+</template>
+
+<script lang="ts" setup></script>
+
+<style></style>
